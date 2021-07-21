b0f9574e9ef95de974a0e6d7e0c35ca010be2e0d
PAsyncronous <anand.priyanka.er@gmail.com>
2021-07-21 09:44:00 +0530

    Datepicker demo app

---
+++ WPFtestApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFtestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private DateTime _StartDate;
+
+        public DateTime StartDate
+        {
+            get { return _StartDate; }
+            set { _StartDate = value; }
+        }
+
+       
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            var date = Datepicker.SelectedDate;
+        }
+
+        private void Datepicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
