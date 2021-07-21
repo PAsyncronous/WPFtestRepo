92105754df6ee5ff70208de50ec0e36b9dad4089
PAsyncronous <anand.priyanka.er@gmail.com>
2021-07-21 14:29:24 +0530

    Datetime feature

---
+++ WPFtestApp/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPFtestApp
+{
+    public class MainWindowViewModel
+    {
+        private ICommand _leftButtonDownCommand;
+
+        public ICommand LeftMouseButtonDown
+        {
+            get
+            {
+                return _leftButtonDownCommand ?? (_leftButtonDownCommand = new RelayCommand(
+                   x =>
+                   {
+                       DoStuff();
+                   }));
+            }
+        }
+
+        private static void DoStuff()
+        {
+            MessageBox.Show("Responding to left mouse button click event...");
+        }
+    }
+}
